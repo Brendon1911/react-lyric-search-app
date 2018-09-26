89c0009889ebcb2f6ca0c193683f47f2737d3ae2
Brendon Richards <brendonrichards93@gmail.com>
2018-09-26 06:30:11 +0000

    Initial index and tracks component setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
 import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Navbar';
+
 import './App.css';
 
 class App extends Component {
