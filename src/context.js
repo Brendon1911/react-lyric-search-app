ecfdbe886465b3093539fbe42902c2e52d96f76c
Brendon Richards <brendonrichards93@gmail.com>
2018-09-26 09:20:36 +0000

    Display tracks setup

--- src/context.js
+++ src/context.js
@@ -6,7 +6,7 @@ const Context = React.createContext();
 export class Provider extends Component {
   state = {
     track_list: [],
-    Heading: 'Top 10 Tracks'
+    heading: 'Top 10 Tracks'
   }
   
   componentDidMount () {
