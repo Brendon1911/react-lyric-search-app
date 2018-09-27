93b97b5eefab10fbe354ed55de9de64333034f09
Brendon Richards <brendonrichards93@gmail.com>
2018-09-27 10:56:07 +0000

    Add to Search component

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -10,7 +10,16 @@ class Search extends Component {
   render () {
     return (
       <Consumer>
-        
+        {value => {
+          return (
+            <div className="card card-body mb-4 p-4">
+              <h1 className="display-4 text-center">
+                <i className="fas fa-music"></i> Search For A Song
+              </h1>
+              <p className="lead text-center">Get the lyrics for any song</p>
+            </div>
+          );
+        }}
       </Consumer>
     );
   }
