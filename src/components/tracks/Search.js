21da0dfee35557e64dfc472e018e84448958815a
Brendon Richards <brendonrichards93@gmail.com>
2018-09-27 10:48:19 +0000

    Initial Search component setup

---
+++ src/components/tracks/Search.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Consumer } from '../../context';
+
+class Search extends Component {
+  state = {
+    trackTitle: ''
+  }
+  
+  render () {
+    return (
+      <Consumer>
+        
+      </Consumer>
+    );
+  }
+}
+
+export default Search;
